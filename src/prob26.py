38e343c711f49f75c5df81a64d4216859b33e9d9
Michael Gurvich <misha.gurvich@gmail.com>
2015-07-26 01:30:58 -0400

    more problems

--- src/prob26.py
+++ src/prob26.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python
 
 import sys
+import time
 
 def reciprocal(n):
     r = 1
@@ -12,13 +13,36 @@ def reciprocal(n):
 
         yield r / n
         r = r % n
+
+def calc_interval(string):
+    fragment = string[10:15]
+    idx = string.find(fragment, 15)
+    return idx - 10
     
-
 if __name__ == "__main__":
-    n = int(sys.argv[1])
-    for i, d in enumerate(reciprocal(n)):
-        print  d,
-        if i > 1000:
-            break
+    start = time.time()
+    
+    N = int(sys.argv[1])
+    precision = int(sys.argv[2])
 
+    max_interval = -100
+    max_num = 0
+    
+    for n in range(1,N):
+        digits = []
+        # print "%d => " %n,
+        for i, d in enumerate(reciprocal(n)):
+            digits.append(d)
+            if i >= precision - 1:
+                break
+            
+        string = ''.join([str(d) for d in digits])
+        interval = calc_interval(string)
+        # print interval
+        if interval > max_interval:
+            max_interval = interval
+            max_num = n
+
+    print "%d ==> %d" %(max_num, max_interval)
+    print time.time() - start
     exit(0)
---
+++ src/prob50.py
@@ -0,0 +1,65 @@
+#!/usr/bin/python
+
+import time
+import sys
+import primes
+
+plist = []
+
+def get_max_sequential_sum_len(p, seq):
+    # print seq
+    end = len(seq) - 1
+    start = end
+    curr_sum = seq[end]
+    max_seq_len = end - start + 1
+    
+    while True:        
+        if curr_sum < p:
+            start -= 1
+            if start < 0: 
+                break
+            curr_sum += seq[start]
+
+        if curr_sum > p:
+            curr_sum -= seq[end]
+            end -= 1
+            if end < 0:
+                break
+            
+        if curr_sum == p:
+            #print "Got seq summing to p: %s -> %d" %(seq[start:end+1], sum(seq[start:end+1]))
+            
+            if end - start + 1 > max_seq_len:
+                max_seq_len = end - start + 1
+                
+            curr_sum -= seq[end]
+            end -= 1
+            start -= 1
+            if start < 0:
+                break
+            curr_sum += seq[start]
+
+    return max_seq_len
+
+if __name__ == "__main__":
+    start = time.time()
+    
+    N = int(sys.argv[1])
+    
+    for p in primes.primes():
+        if p >= N:
+            break
+        plist.append(p)
+
+    max_seq_length = 0
+    max_prime = 0
+    for i,p in enumerate(plist[::-1]):
+        seq_length = get_max_sequential_sum_len(p, plist[:len(plist)-i])
+        print "%d ==> %d" %(p, seq_length)        
+        if seq_length > max_seq_length:
+            max_prime = p
+            max_seq_length = seq_length
+
+            
+    print "%d ==> %d" %(max_prime, max_seq_length)
+    print time.time() - start
