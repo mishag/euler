a8f39d69f8468fe41963103543709441c146fe6e
Michael Gurvich <misha.gurvich@gmail.com>
2015-08-09 18:48:58 -0400

    cosmetic changes

--- src/prob59.py
+++ src/prob59.py
@@ -7,9 +7,7 @@ def decode(line, key):
     N = len(line)
     for i in range(N):
         s += "%s" %chr(line[i] ^ key[i % 3])
-        # s += "%s%s%s"  %(chr(key[0] ^ line[3*i]),
-        #                  chr(key[1] ^ line[3*i + 1]),
-        #                  chr(key[2] ^ line[3*i + 2]))
+        
     return s
 
 if __name__ == "__main__":
